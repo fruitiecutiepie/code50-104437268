39273b8f34096d44568c46b8cda2e3c7a879c19c
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-05-28 06:45:55 +0000

    Sat, May 28, 2022, 6:45 AM +00:00

--- hello.c
+++ hello.c
@@ -3,7 +3,8 @@
 
 int main(void)
 {
-    string answer = get_string("What's your name? ");
-    printf("Hello, %s\n", answer);
+    string first_name = get_string("What's your first name? ");
+    string last_name = get_string("What's your last name? ");
+    printf("Hello, %s %s\n", first_name, last_name);
 }
 
