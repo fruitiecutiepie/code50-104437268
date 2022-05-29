b2828cea72c70e3cf4061fdf733be4cbb5ffaa2f
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-05-29 05:17:53 +0000

    Sun, May 29, 2022, 5:17 AM +00:00

--- agree.c
+++ agree.c
@@ -3,5 +3,16 @@
 
 int main(void)
 {
-    
+    // Prompt user to agree
+    char c = get_char("Do you agree? ");
+
+    // Check whether user agreed
+    if (c == 'y' or 'N')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n' or 'N')
+    {
+        printf("Not agreed.\n");
+    }
 }
