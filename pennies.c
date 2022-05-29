03ee63d6f62a55a6119477e03844f511110e3df6
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-05-29 07:00:10 +0000

    Sun, May 29, 2022, 7:00 AM +00:00

--- pennies.c
+++ pennies.c
@@ -1,9 +1,10 @@
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
 
 int main(void)
 {
     float amount = get_float("Dollar amount: ");
-    int pennies = amount * 100;
+    int pennies = round(amount * 100);
     printf("Pennies: %i\n", pennies);
 }
