4b3104df79b19e7775decbaf42f21b4340b4df77
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-05-29 05:05:37 +0000

    Sun, May 29, 2022, 5:05 AM +00:00

--- calculator.c
+++ calculator.c
@@ -14,7 +14,14 @@ int main(void)
 
     if (x < y)
     {
-        printf
+        printf("x is less than y\n");
     }
-
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
 }
