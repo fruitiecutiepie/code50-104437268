147631eee284dbea663d71b31e6b11f42f3a7079
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-06-14 03:48:15 +0000

    Tue, Jun 14, 2022, 3:48 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,5 +4,12 @@
 int main(void)
 {
     long credit_number = get_long("Number: ");
-    
+    do
+    {
+        credit_num = get_long("Number: ");
+    }
+    while
+    {
+        credit_num != 
+    }
 }
