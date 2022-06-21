f278fa7b3c03f21d5e5135075923e71d643cb97d
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-06-21 10:55:42 +0000

    Tue, Jun 21, 2022, 10:55 AM +00:00

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -24,5 +24,8 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    for (i = 0, w = strlen(word))
+    for (i = 0, w = strlen(word); i < w; i++)
+    {
+        
+    }
 }
