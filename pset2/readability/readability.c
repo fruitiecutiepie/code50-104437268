4058cb30b58945d4581f1b4c69262e1b1e06a31e
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-06-22 08:09:14 +0000

    Wed, Jun 22, 2022, 8:09 AM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -1,8 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+
+int count_letters(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
-    printf("%s\n", text);
+    letters = count_letters(text);
+    printf("%i\n", letters);
+}
+
+int count_letters(string text)
+{
+
 }
