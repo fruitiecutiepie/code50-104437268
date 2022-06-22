077557f1be7384a913ea4e7fd69dc7536c3b043a
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-06-22 09:32:38 +0000

    Wed, Jun 22, 2022, 9:32 AM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -4,12 +4,13 @@
 int main(int argc, string argv[])
 {
     // Validate key
+    
+
     // Check key length
     // Check for non-alphabetic characters
     // Check for repeated characters (case-insensitive)
 
     // Get plaintext
-    
 
     // Encipher
 
