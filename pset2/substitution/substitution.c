fe87f7ffbc7004bdb4a0ca4adf72255e34b5e1df
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-06-22 09:27:22 +0000

    Wed, Jun 22, 2022, 9:27 AM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    // Get key
 
+    // Validate key
+
+    // Get plaintext
+
+    // Encipher
+
+    // Print ciphertext
 }
