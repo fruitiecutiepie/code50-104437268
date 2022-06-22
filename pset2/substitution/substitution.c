509128d62e5f3c93db29233c2e21c9e2710bc6de
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-06-22 09:39:55 +0000

    Wed, Jun 22, 2022, 9:39 AM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -1,5 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -11,7 +13,14 @@ int main(int argc, string argv[])
     }
 
     // Check key length
+    if (strlen(argv[1]) != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+
     // Check for non-alphabetic characters
+    if (isalpha(argv[1]))
     // Check for repeated characters (case-insensitive)
 
     // Get plaintext
