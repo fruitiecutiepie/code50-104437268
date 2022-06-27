3383e5a1dee2a38fa3255da794212fb3f1029924
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-06-27 09:11:05 +0000

    Mon, Jun 27, 2022, 9:11 AM +00:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -169,7 +169,11 @@ bool print_winner(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (voter_count / 2)
+        if (candidates[i].votes >= voter_count / 2 + 1)
+        {
+            printf("%s", candidates[i].name);
+            return true;
+        }
     }
     return false;
 }
