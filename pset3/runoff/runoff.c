006730a14b5907090dcab6895648c0c4b573fa45
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-06-27 08:16:54 +0000

    Mon, Jun 27, 2022, 8:16 AM +00:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -133,7 +133,7 @@ bool vote(int voter, int rank, string name)
     {
         for (j = 0; j < candidate_count; j++)
         {
-            
+            candidates
         }
     }
     return false;
