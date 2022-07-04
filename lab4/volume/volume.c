685b87febb05cb64205d8cd769c6e8c0c96b5fdd
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-04 04:34:42 +0000

    Mon, Jul 4, 2022, 4:34 AM +00:00

--- lab4/volume/volume.c
+++ lab4/volume/volume.c
@@ -38,8 +38,8 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     for (int i = 0; i < HEADER_SIZE; i++)
     {
-        fread(input, sizeof(uint8_t), 1, input);
-        fwrite(input, sizeof(uint8_t), 1, input);
+        fread(header, sizeof(uint8_t), 1, input);
+        fwrite(input, sizeof(uint8_t), 1, output);
     }
 
     // TODO: Read samples from input file and write updated data to output file
