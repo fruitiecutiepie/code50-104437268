7c8815bc35b94b9735373286b347c457bfce8ae9
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-04 05:08:33 +0000

    Mon, Jul 4, 2022, 5:08 AM +00:00

--- lab4/volume/volume.c
+++ lab4/volume/volume.c
@@ -44,7 +44,6 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     while (fread(&buffer, sizeof(int16_t), 1, input))
     {
-        fread(&buffer, sizeof(int16_t), 1, input);
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
     }
