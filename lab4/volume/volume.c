ffe00c0c6655db85be1def7263fce005a9bf49a3
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-04 05:11:41 +0000

    Mon, Jul 4, 2022, 5:11 AM +00:00

--- lab4/volume/volume.c
+++ lab4/volume/volume.c
@@ -34,8 +34,6 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
     uint8_t header[HEADER_SIZE];
     int16_t buffer;
-    fseek(input, 0L, SEEK_END);
-    long int input_size = ftell(input);
 
     // TODO: Copy header from input file to output file
     fread(header, sizeof(uint8_t), HEADER_SIZE, input);
