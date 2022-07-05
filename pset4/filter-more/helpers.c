03da960f44af14e6919749aa9acfc14d7cf48d8a
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-05 16:35:34 +0000

    Tue, Jul 5, 2022, 4:35 PM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -25,7 +25,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width / 2; j++)
         {
             RGBTRIPLE tmp = image[i][j];
-            image[i][j] = image[i][width - j];
+            image[i][j] = image[i][width - j - 1];
             image[i][width - j] = tmp;
         }
     }
