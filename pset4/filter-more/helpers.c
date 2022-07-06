f11e6011aabe0751c687a9705fea0150a8957470
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-06 03:25:33 +0000

    Wed, Jul 6, 2022, 3:25 AM +00:00

--- pset4/filter-more/helpers.c
+++ pset4/filter-more/helpers.c
@@ -27,7 +27,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width / 2; j++)
         {
-            // Swap each pixel value with 
+            // Swap the value of each pixel with that of its equivalent
             RGBTRIPLE tmp = image[i][j];
             image[i][j] = image[i][width - j - 1];
             image[i][width - j - 1] = tmp;
