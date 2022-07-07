371c3b5a1b0916d2d1a96ca9478b1989a8b5663e
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-07 05:59:59 +0000

    Thu, Jul 7, 2022, 5:59 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             counter++;
-            filename 
+            char *filename = malloc(sizeof())
             sprintf(filename, %03i.jpg, counter);
         }
     }
