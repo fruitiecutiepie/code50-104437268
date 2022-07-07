13170bae61e6e4a689fb3484dd9b9a23d6c85986
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-07 06:46:27 +0000

    Thu, Jul 7, 2022, 6:46 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -47,7 +47,10 @@ int main(int argc, char *argv[])
         }
         else
         {
-            fwrite(buffer, 1, BLOCK_SIZE, img);
+            if ()
+            {
+                fwrite(buffer, 1, BLOCK_SIZE, img);
+            }
         }
     }
     fclose(file);
