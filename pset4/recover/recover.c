37eac413a72e2091f238828c9cc200b15e15cd01
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-07 07:19:23 +0000

    Thu, Jul 7, 2022, 7:19 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            if (img == true)
+            if (counter > 0)
             {
                 fclose(img);
             }
