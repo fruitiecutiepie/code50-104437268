98c03665ac77720b142fbd4600eaf785f4374c45
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-07 05:13:29 +0000

    Thu, Jul 7, 2022, 5:13 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 1)
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+
+    if (FILE *file = fopen(argv[1], "r") == NULL)
+    {
+        printf("File cannot be opened.\n");
+        return 2;
+    }
 }
