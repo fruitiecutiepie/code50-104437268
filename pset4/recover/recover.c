6b0e99806a64e761fdc3e43ca093a548f08b8294
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-07 05:54:08 +0000

    Thu, Jul 7, 2022, 5:54 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -22,13 +22,16 @@ int main(int argc, char *argv[])
 
     int BLOCK_SIZE = 512;
     BYTE buffer[BLOCK_SIZE];
+    int counter = 0;
 
     // Read input file until the end
     while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            
+            counter++;
+            filename 
+            sprintf(filename, %03i.jpg, counter);
         }
     }
 }
