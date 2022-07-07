a1429ac82f671330ef03dd2ae75dfebfb91bdffb
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-07 06:43:42 +0000

    Thu, Jul 7, 2022, 6:43 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -28,6 +28,8 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            fclose(img);
+
             // Store formatted output file name in filename
             char *filename = malloc(8);
             sprintf(filename, "%03i.jpg", counter);
@@ -42,8 +44,6 @@ int main(int argc, char *argv[])
 
             // Free filename memory
             free(filename);
-
-            if
         }
         else
         {
