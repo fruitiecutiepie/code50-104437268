1054019ef4a547f4f5f20725d6d5c70e520aac82
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-11 05:20:16 +0000

    Mon, Jul 11, 2022, 5:20 AM +00:00

--- lab5/inheritance/inheritance.c
+++ lab5/inheritance/inheritance.c
@@ -56,7 +56,7 @@ person *create_family(int generations)
         person->parent0
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-
+        person.alleles
     }
 
     // If there are no generations left to create
