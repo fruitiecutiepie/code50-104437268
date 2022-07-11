227a17a619693d006f7eb8d655d589ce486af63d
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-11 08:36:12 +0000

    Mon, Jul 11, 2022, 8:36 AM +00:00

--- lab5/inheritance/inheritance.c
+++ lab5/inheritance/inheritance.c
@@ -54,20 +54,20 @@ person *create_family(int generations)
         p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p.alleles[0] = p->parents[0].alleles[rand() % 2];
-        p.alleles[1] = p->parents[1].alleles[rand() % 2];
+        p->alleles[0] = p->parents[0]->alleles[rand() % 2];
+        p->alleles[1] = p->parents[1]->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        p->parent[0] = NULL;
+        p->parents[0] = NULL;
         p->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        p.alleles[0] = random_allele();
-        p.alleles[1] = random_allele();
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
