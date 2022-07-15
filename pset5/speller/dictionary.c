e37765693384dea4599670ac98d387507252119b
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-15 15:00:48 +0000

    Fri, Jul 15, 2022, 3:00 PM +00:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -111,11 +111,10 @@ bool load(const char *dictionary)
         //     n->next = table[*i[0]][*i[1]][*i[2]]->word;
         //     table[*i[0]][*i[1]][*i[2]]->next = n;
         // }
-        strcpy(n->next, table[i]->next);
         strcpy(table[i]->word, n->word);
-
-        return true;
+        n->next = table[i]->next;
     }
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
