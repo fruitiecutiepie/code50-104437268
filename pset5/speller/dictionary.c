885927d6c5efa1a7524e50543615a4174a2307e1
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-15 14:49:44 +0000

    Fri, Jul 15, 2022, 2:49 PM +00:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -111,8 +111,8 @@ bool load(const char *dictionary)
         //     n->next = table[*i[0]][*i[1]][*i[2]]->word;
         //     table[*i[0]][*i[1]][*i[2]]->next = n;
         // }
-        table[i]->word = n->word;
-        n->next = table[i]->next;
+        n->next = table[i]->word;
+        table[i]->next = n;
 
         return true;
     }
