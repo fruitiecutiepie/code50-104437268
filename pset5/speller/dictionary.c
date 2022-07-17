025311bb6c6622bb81a70252ef57ac9f35217b66
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-17 08:48:11 +0000

    Sun, Jul 17, 2022, 8:48 AM +00:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -127,14 +127,14 @@ bool unload(void)
     {
         // Set cursor & tmp to first word in dictionary
         node *cursor = table[i];
-        node *tmp = table[i];
+        // node *tmp = table[i];
 
         // Free any memory allocated in load
         while (cursor != NULL)
         {
             cursor = cursor->next;
-            free(tmp);
-            tmp = cursor;
+            // free(tmp);
+            // tmp = cursor;
         }
     }
     return true;
