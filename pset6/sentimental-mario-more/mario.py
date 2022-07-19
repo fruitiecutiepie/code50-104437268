e89cd7a3b583cda66cca5eaf944dacd548ead680
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-19 16:41:43 +0000

    Tue, Jul 19, 2022, 4:41 PM +00:00

--- pset6/sentimental-mario-more/mario.py
+++ pset6/sentimental-mario-more/mario.py
@@ -3,5 +3,5 @@ from cs50 import get_int
 
 while True:
     height = get_int("Height: ")
-    if height != range(1,8):
-        height = get_int("Height: ")
+    if height > 0 and height < 9:
+        break
