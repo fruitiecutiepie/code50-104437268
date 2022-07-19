a1e476b9a5689dad87782d728ca079b17e2bba14
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-19 17:02:35 +0000

    Tue, Jul 19, 2022, 5:02 PM +00:00

--- pset6/sentimental-mario-more/mario.py
+++ pset6/sentimental-mario-more/mario.py
@@ -1,14 +1,22 @@
 # TODO
 from cs50 import get_int
 
+# Prompt user for a positive integer between 1 & 8, inclusive
 while True:
     height = get_int("Height: ")
     if height > 0 and height < 9:
         break
 
 for i in range(1, height + 1):
+    # Print left whitespace
     for j in range(height - i, 0, -1):
         print(" ", end="")
+
+    # Print left pyramid
     print("#" * i, end="")
+
+    # Print middle whitespace
     print("  ", end="")
+
+    # Print right pyramid
     print("#" * i)
