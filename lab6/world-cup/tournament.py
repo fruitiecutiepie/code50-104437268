6f296f54f1bcba4767eb288354f9ffc81d77fa5d
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-19 14:14:03 +0000

    Tue, Jul 19, 2022, 2:14 PM +00:00

--- lab6/world-cup/tournament.py
+++ lab6/world-cup/tournament.py
@@ -16,7 +16,7 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    with open(sys.argv[1]) as file:
+    with open(sys.argv[1], "r") as file:
         reader = csv.DictReader(file)
         teams.append()
 
