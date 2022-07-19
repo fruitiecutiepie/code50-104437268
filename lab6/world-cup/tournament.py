d3173e1b006f152f64b0fe5317671f6043ab5f06
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-19 14:40:11 +0000

    Tue, Jul 19, 2022, 2:40 PM +00:00

--- lab6/world-cup/tournament.py
+++ lab6/world-cup/tournament.py
@@ -61,7 +61,8 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    simulate_round(teams)
+    while len(simulate_round(teams)) != 1:
+        
 
 
 if __name__ == "__main__":
