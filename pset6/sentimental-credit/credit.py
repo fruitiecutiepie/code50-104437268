081048ee390a0c1356537921148d2c540b4b2f14
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-20 04:56:35 +0000

    Wed, Jul 20, 2022, 4:56 AM +00:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -1,4 +1,5 @@
 # TODO
+# Prompt user for card number
 while True:
     num = input("Number: ")
     try:
@@ -22,12 +23,12 @@ for i in range(len(num)-2, -1, -2):
 for i in range(len(num)-1, -1, -2):
     total += int(num[i])
 
-# Print output according to each number
+# Print output according to each card number
 if str(total)[-1] == '0':
     if len(num) == 15 and num[:2] == '34' or num[:2] == '37':
         print("AMEX")
     if len(num) == 16 and num[:2] == '51' or num[:2] == '52' or \
-        num[:2] == '53' or num[:2] == '54' or num[:2] == '55':
+            num[:2] == '53' or num[:2] == '54' or num[:2] == '55':
         print("MASTERCARD")
     if len(num) == 13 or len(num) == 16 and num[0] == '4':
         print("VISA")
