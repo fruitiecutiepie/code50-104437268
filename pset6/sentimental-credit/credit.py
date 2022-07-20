bec7c5b019fca154565f56b1159715fd080139c9
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-20 04:25:43 +0000

    Wed, Jul 20, 2022, 4:25 AM +00:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -6,7 +6,9 @@ total = 0
 for i in range(len(num)-2, -1, -2):
     mult = num[i] * 2
     for j in mult:
-        total +=
+        total += j
 
 for i in range(len(num)-1, -1, -2):
-    print(num[i])
+    total += num[i]
+
+print(total)
