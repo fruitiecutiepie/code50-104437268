b56f277fe8d471017341fadbc19ca812102a6875
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-21 15:14:33 +0000

    Thu, Jul 21, 2022, 3:14 PM +00:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -10,8 +10,7 @@ def main():
 
     # TODO: Read database file into a variable
     with open(sys.argv[1], 'r') as f:
-        next(f)
-        database = csv.reader(f)
+        database = csv.DictReader(f)
 
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2], 'r') as f:
