86d564ec9e4b93ac81a112e6eee3ad214284e5af
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-21 14:44:21 +0000

    Thu, Jul 21, 2022, 2:44 PM +00:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -5,14 +5,15 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if len(sys.argv) != 2
+    if len(sys.argv) != 3:
+        print('Usage: python dna.py data.csv sequence.txt')
 
     # TODO: Read database file into a variable
-    with open(sys.argv[1], 'r') as database:
+    # with open(sys.argv[1], 'r') as database:
 
 
     # TODO: Read DNA sequence file into a variable
-    with open(sys.argv[2], 'r') as dna:
+    # with open(sys.argv[2], 'r') as dna:
 
     # TODO: Find longest match of each STR in DNA sequence
 
