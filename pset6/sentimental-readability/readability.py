1fd730d481329cf19d221d52cc146239226210fc
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-21 13:08:57 +0000

    Thu, Jul 21, 2022, 1:08 PM +00:00

--- pset6/sentimental-readability/readability.py
+++ pset6/sentimental-readability/readability.py
@@ -8,12 +8,18 @@ words = 1
 sentences = 0
 
 for i in text:
+    # Count letters
     if text[i].isalpha():
         letters += 1
+
+    # Count words
     if text[i].isspace():
         words += 1
+
+    # Count sentences
     if text[i] == '.' or text[i] == '!' or text[i] == '?':
         sentences += 1
+
 
 index = 0.0588 * L - 0.296 * S - 15.8
 grade = round(index)
