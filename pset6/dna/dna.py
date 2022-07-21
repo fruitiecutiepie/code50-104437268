5c5e239685a7c1b96930105ff8b37e891507260b
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-21 14:52:02 +0000

    Thu, Jul 21, 2022, 2:52 PM +00:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -10,11 +10,11 @@ def main():
 
     # TODO: Read database file into a variable
     with open(sys.argv[1], 'r') as file:
-        database = csv.reader(file)
-
+        database = csv.DictReader(file)
 
     # TODO: Read DNA sequence file into a variable
-    # with open(sys.argv[2], 'r') as dna:
+    with open(sys.argv[2], 'r') as file:
+        dna = 
 
     # TODO: Find longest match of each STR in DNA sequence
 
