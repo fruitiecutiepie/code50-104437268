872ed032482d15edc1b64aa85f3b02444c2b9898
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-21 15:18:23 +0000

    Thu, Jul 21, 2022, 3:18 PM +00:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -7,6 +7,7 @@ def main():
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
         print('Usage: python dna.py data.csv sequence.txt')
+        return
 
     # TODO: Read database file into a variable
     with open(sys.argv[1], 'r') as f:
