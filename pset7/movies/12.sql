a83780f7f69a39e4716480623f516427c0a61f81
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-26 08:45:57 +0000

    Tue, Jul 26, 2022, 8:45 AM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,5 +1,4 @@
-SELECT title, person_id FROM movies
+SELECT title, stars.person_id FROM movies
 JOIN stars
 ON stars.movie_id = movies.id
-WHERE stars.person_id = (SELECT id FROM people WHERE name = 'Johnny Depp')
-AND stars.person_id = (SELECT id FROM people WHERE name = 'Helena Bonham Carter');
+WHERE stars.person_id = (SELECT id FROM people WHERE name = 'Johnny Depp' AND (SELECT id FROM people WHERE name = 'Helena Bonham Carter'));
