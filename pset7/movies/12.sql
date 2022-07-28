3facbe195204d33639e9d59a2e5f63c32cd135e3
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-28 04:47:13 +0000

    Thu, Jul 28, 2022, 4:47 AM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,13 +1,11 @@
-SELECT DISTINCT title FROM
-
-(SELECT title, name FROM movies
-JOIN stars JOIN people
-ON stars.movie_id = movies.id AND stars.person_id = people.id
-WHERE name = 'Johnny Depp'
-UNION
 SELECT title, name FROM movies
 JOIN stars JOIN people
 ON stars.movie_id = movies.id AND stars.person_id = people.id
-WHERE name = 'Helena Bonham Carter')
+WHERE name = 'Johnny Depp'
 
-HAVING COUNT(title) = 2;
+INTERSECT
+
+SELECT title, name FROM movies
+JOIN stars JOIN people
+ON stars.movie_id = movies.id AND stars.person_id = people.id
+WHERE name = 'Helena Bonham Carter';
