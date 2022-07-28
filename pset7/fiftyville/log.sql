1bd4d1240754c83c100bf4748f2dfd9021c3cd24
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-28 05:50:12 +0000

    Thu, Jul 28, 2022, 5:50 AM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -1,8 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT *
+SELECT description
 FROM crime_scene_reports
 WHERE year = 2021
 AND month = 7
 AND day = 28
-AND street = "Humphrey Street";
+AND street = 'Humphrey Street';
