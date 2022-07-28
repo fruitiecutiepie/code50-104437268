c8bdc175ae098b6e5fe8c7af02554b5c1a9f5fbd
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-28 04:59:43 +0000

    Thu, Jul 28, 2022, 4:59 AM +00:00

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,8 +1,8 @@
-SELECT name FROM movies
+SELECT DISTINCT(name) FROM movies
 JOIN stars JOIN people
 ON stars.movie_id = movies.id
 AND stars.person_id = people.id
-WHERE movies.id =
+WHERE movies.id IN
 (
     SELECT movies.id FROM movies
     JOIN stars JOIN people
@@ -11,3 +11,11 @@ WHERE movies.id =
     WHERE name = "Kevin Bacon"
     AND birth = 1958
 )
+EXCEPT
+
+SELECT name FROM movies
+JOIN stars JOIN people
+ON stars.movie_id = movies.id
+AND stars.person_id = people.id
+WHERE name = "Kevin Bacon"
+AND birth = 1958;
