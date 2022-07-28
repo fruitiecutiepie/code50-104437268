eb3fc5c96acca331bb402d7e9b00d548268e63aa
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-28 04:33:46 +0000

    Thu, Jul 28, 2022, 4:33 AM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -2,7 +2,7 @@ SELECT title, name FROM movies
 JOIN stars JOIN people
 ON stars.movie_id = movies.id AND stars.person_id = people.id
 WHERE name = 'Johnny Depp'
-INTERSECT
+UNION
 SELECT title, name FROM movies
 JOIN stars JOIN people
 ON stars.movie_id = movies.id AND stars.person_id = people.id
