1838ab12d6674de88f2f993c38fcb37d27c74597
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-28 06:29:36 +0000

    Thu, Jul 28, 2022, 6:29 AM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -15,7 +15,7 @@ FROM interviews
 WHERE year = 2021
 AND month = 7
 AND day = 28;
--- Found witnesses: Ruth, Eugene, and Raymond
+-- Found witnesses: Ruth, Eugene, and Raymond.
 
 -- Look for cars that exited the bakery within ten minutes of the theft
 SELECT license_plate
@@ -26,16 +26,17 @@ AND day = 28
 AND hour = 10
 AND minute BETWEEN 10 AND 20
 AND activity = 'exit';
--- Found possible license plate: 5P2BI95, 94KL13X, 6P58WS2, 4328GD8, or G412CB7
+-- Found possible license plate: 5P2BI95, 94KL13X, 6P58WS2, 4328GD8, or G412CB7.
 
 -- Look for ATM transactions on Leggett Street earlier that morning
-SELECT *
+SELECT account_number, amount
 FROM atm_transactions
 WHERE year = 2021
 AND month = 7
 AND day = 28
 AND atm_location = 'Leggett Street'
-AND transaction_type = 
+AND transaction_type = 'withdraw';
+-- Found 8 possible account numbers.
 
 -- Look for phone calls for < 1 minute as the thief was leaving the bakery -> accomplice
 
