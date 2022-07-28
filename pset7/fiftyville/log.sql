6bb7fd484d93e673e3d8fe0494d73504f944b7e0
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-28 05:53:04 +0000

    Thu, Jul 28, 2022, 5:53 AM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -1,5 +1,6 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Look for a crime scene report that matches the date and the location of the crime
 SELECT *
 FROM crime_scene_reports
 WHERE year = 2021
