fa9127369eef93b56df550572ddba32ea327e1a6
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-28 05:34:10 +0000

    Thu, Jul 28, 2022, 5:34 AM +00:00

--- pset7/movies/4.sql
+++ pset7/movies/4.sql
@@ -1,3 +1,5 @@
-SELECT COUNT(title) FROM movies
+SELECT COUNT(title)
+FROM movies
     JOIN ratings
-    WHERE rating = 10.0 AND ratings.movie_id = movies.id;
+    WHERE rating = 10.0
+    AND ratings.movie_id = movies.id;
