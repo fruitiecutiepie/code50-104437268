cfb7146233535fc90fc780e90201ce17e8534721
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-28 04:32:49 +0000

    Thu, Jul 28, 2022, 4:32 AM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,4 +1,9 @@
 SELECT title, name FROM movies
 JOIN stars JOIN people
 ON stars.movie_id = movies.id AND stars.person_id = people.id
-WHERE name IN ('Johnny Depp', 'Helena Bonham Carter');
+WHERE name = 'Johnny Depp'
+INTERSECT
+SELECT title, name FROM movies
+JOIN stars JOIN people
+ON stars.movie_id = movies.id AND stars.person_id = people.id
+WHERE name = 'Helena Bonham Carter';
