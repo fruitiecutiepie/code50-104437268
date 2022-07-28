e62786f7793b63e5cdf7c07d2fe9b4a1c9c3001e
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-07-28 06:41:19 +0000

    Thu, Jul 28, 2022, 6:41 AM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -50,7 +50,7 @@ AND duration < 60;
 -- Look for people with aforementioned license plates
 SELECT *
 FROM people
-WHERE license_plate IN ('5P2BI95', '94KL13X', '6P58WS2', '4328GD8', 'G412CB7');
+WHERE license_plate IN ('5P2BI95', '94KL13X', '6P58WS2', '4328GD8', 'G412CB7')
 
 INTERSECT
 
