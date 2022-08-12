d1a34705a164e566d965d07503a07b5ec92e0707
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-08-12 16:21:25 +0000

    Fri, Aug 12, 2022, 4:21 PM +00:00

--- pset8/homepage/scripts.js
+++ pset8/homepage/scripts.js
@@ -1,8 +1,5 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "300px";
-    if (window.matchMedia("(max-width: < 450px)")) {
-        document.getElementById("mySidenav").style.width = "100%";
-    }
 }
 
 function closeNav() {
