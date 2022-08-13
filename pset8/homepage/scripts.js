b77dbb8242042c4ac1552976588957641b4f2f7f
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-08-13 05:16:52 +0000

    Sat, Aug 13, 2022, 5:16 AM +00:00

--- pset8/homepage/scripts.js
+++ pset8/homepage/scripts.js
@@ -7,8 +7,9 @@ function closeNav() {
 }
 
 function openImage() {
-    document.getElementByTagName("img").classList.add = "center";
+    document.getElementByTagName("img").classList.add('translate-middle');
 }
+
 // document.addEventListener('DOMContentLoaded', function {
 //     document.querySelector('.images').addEventListener('click', function {
 //         document.body.style.backgroundColor = hsl(0, 0%, 50%);
