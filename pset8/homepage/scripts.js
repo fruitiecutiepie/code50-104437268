6f2d415780fa95fa85e147a9ed0b3c2778f7b25b
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-08-13 05:10:40 +0000

    Sat, Aug 13, 2022, 5:10 AM +00:00

--- pset8/homepage/scripts.js
+++ pset8/homepage/scripts.js
@@ -7,7 +7,10 @@ function closeNav() {
 }
 
 function openImage() {
-    document.getElementByTagName("img").style.
+    document.getElementByTagName("img").style.textAlign = "center";
+    document.getElementByTagName("img").style.margin = "auto";
+    document.getElementByTagName("img").style.display = "block";
+    document.getElementByTagName("img").style.width = "60%";
 }
 // document.addEventListener('DOMContentLoaded', function {
 //     document.querySelector('.images').addEventListener('click', function {
