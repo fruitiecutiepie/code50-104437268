04c60565d7f4cc17bbc8fce0960ee37ef6a5c814
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-08-13 06:57:10 +0000

    Sat, Aug 13, 2022, 6:57 AM +00:00

--- pset8/homepage/scripts.js
+++ pset8/homepage/scripts.js
@@ -6,8 +6,9 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-function openImage() {
-    document.querySelector("img").classList.add('position-absolute', 'top-50', 'start-0', 'translate-middle');
-    document.querySelector("img").style.objectFit = "scale-down";
-    document.querySelector("body").style.backgroundColor = "#808080";
-}
+//  Simulate modal image
+//  function openImage() {
+//     document.querySelector("img").classList.add('top-50', 'start-0', 'translate-middle');
+//     document.querySelector("img").style.objectFit = "scale-down";
+//     document.querySelector("body").style.backgroundColor = "#808080";
+// }
