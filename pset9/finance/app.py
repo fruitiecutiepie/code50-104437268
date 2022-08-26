46653494768398bc96206fd99601d37282c3c66e
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-08-26 13:27:55 +0000

    Fri, Aug 26, 2022, 1:27 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -90,7 +90,8 @@ def buy():
         db.execute("INSERT INTO portfolios (user_id, symbol, shares) VALUES (?, ?, ?)", session["user_id"], quote["symbol"], shares)
 
         # Record purchase
-        db.execute("UPDATE portfolios SET bought = ? AND purchase_price = ? WHERE symbol = ? AND id = ?", shares, session["user_id"])
+        db.execute("UPDATE portfolios SET bought = ? AND purchase_price = ? WHERE symbol = ? AND id = ?", shares, quote["price"], symbol, session["user_id"])
+        
         # Update cash
         db.execute("UPDATE users SET cash = ? WHERE id = ?", (cash - total), session["user_id"])
 
