8d8320dfd905ffebfb6cec6f7c5c7774c017fa12
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-08-26 15:07:21 +0000

    Fri, Aug 26, 2022, 3:07 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -72,7 +72,7 @@ def buy():
 
         # Ensure symbol was submitted
         if not symbol:
-            return apology("must provide symbol", 403)
+            return apology("must provide symbol", 400)
 
         # Ensure symbol exists
         if not quote:
@@ -80,7 +80,7 @@ def buy():
 
         # Ensure shares was submitted
         if not shares or shares < 1:
-            return apology("must provide a positive number of shares", 403)
+            return apology("must provide a positive number of shares", 400)
 
         # Ensure user has enough cash
         if cash < total:
@@ -131,11 +131,11 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
@@ -176,7 +176,7 @@ def quote():
 
         # Ensure symbol was submitted
         if not request.form.get("symbol"):
-            return apology("must provide symbol", 403)
+            return apology("must provide symbol", 400)
 
         # Ensure symbol exists
         if not lookup(request.form.get("symbol")):
@@ -211,7 +211,7 @@ def register():
 
         # Ensure password and password confirmation confirmed
         elif request.form.get("password") != request.form.get("confirmation"):
-            return apology("passwords must match", 403)
+            return apology("passwords must match", 400)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
@@ -254,7 +254,7 @@ def sell():
 
         # Ensure symbol was submitted
         if not symbol:
-            return apology("must provide symbol", 403)
+            return apology("must provide symbol", 400)
 
         # Ensure symbol exists
         if not lookup(symbol):
@@ -266,7 +266,7 @@ def sell():
 
         # Ensure shares was submitted
         if not shares or shares < 1:
-            return apology("must provide a positive number of shares", 403)
+            return apology("must provide a positive number of shares", 400)
 
         # Ensure number of sold shares was valid
         stock_shares = db.execute("SELECT shares FROM portfolios WHERE symbol = ? AND id = ?", symbol, session["user_id"])
