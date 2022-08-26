f7b562fc9beddcd83328843dc678f014049a78b3
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-08-26 15:02:50 +0000

    Fri, Aug 26, 2022, 3:02 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -199,15 +199,15 @@ def register():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
         # Ensure password was confirmed
         elif not request.form.get("confirmation"):
-            return apology("must provide password confirmation", 403)
+            return apology("must provide password confirmation", 400)
 
         # Ensure password and password confirmation confirmed
         elif request.form.get("password") != request.form.get("confirmation"):
