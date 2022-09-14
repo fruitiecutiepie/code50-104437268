2f57bf8c5b285729aa9ae16b36e135a0a20b67d2
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2022-09-14 04:31:53 +0000

    Wed, Sep 14, 2022, 4:31 AM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -227,7 +227,7 @@ def register():
         db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), hash)
 
         # Remember which user has logged in
-        session["user_id"] = db.execute("SELECT id FROM users WHERE username = ?", request.form.get("username"))
+        session["user_id"] = db.execute("SELECT id FROM users WHERE username = ?", request.form.get("username"))["id"]
 
         # Redirect user to look up stock quotes
         return redirect("/quote")
